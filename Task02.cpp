374452f8e00dae809de6aedee434c8ae977d7d13
zohaibofcl <zohaibofcl@gmail.com>
2024-11-15 10:45:13 +0500

    Add files via upload

---
+++ Task02.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            break;
+        default:
+            cout << "Invalid operator! Please use +, -, *, or /." << endl;
+    }
+
+    return 0;
+}
