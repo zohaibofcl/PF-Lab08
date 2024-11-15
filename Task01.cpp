7fedd2771da161231055e37259ff26a9bf9f0847
zohaibofcl <zohaibofcl@gmail.com>
2024-11-15 10:44:47 +0500

    Add files via upload

---
+++ Task01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int day;
+    cout << "Enter a number (1-7): ";
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "Sunday" << endl;
+            break;
+        case 2:
+            cout << "Monday" << endl;
+            break;
+        case 3:
+            cout << "Tuesday" << endl;
+            break;
+        case 4:
+            cout << "Wednesday" << endl;
+            break;
+        case 5:
+            cout << "Thursday" << endl;
+            break;
+        case 6:
+            cout << "Friday" << endl;
+            break;
+        case 7:
+            cout << "Saturday" << endl;
+            break;
+        default:
+            cout << "Invalid input! Please enter a number between 1 and 7." << endl;
+    }
+
+    return 0;
+}
